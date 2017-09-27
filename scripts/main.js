cd3b6ea1e602dfd444744915e3dda0f37f29324a
Guillaume Etendard <guillaumetendard@gmail.com>
2017-09-27 14:58:57 +0200

    Add remaining computed property and pluralize filter

--- scripts/main.js
+++ scripts/main.js
@@ -11,7 +11,21 @@
                     { title: "Manger", isDone: false},
                     { title: "Dormir", isDone: false},
                 ]
-            }
+            },
+
+            filters: {
+                pluralize: function (number, word) {
+                    if(number > 1) word += 's';
+                    return number + ' ' + word;
+                }
+            },
+
+            computed: {
+                remaining: function () {
+                    if(!this.tasks) return;
+                    return this.tasks.filter(task => !task.isDone).length
+                }
+            },
         }
     )
 }
