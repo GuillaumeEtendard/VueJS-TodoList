2130e782626ddb0bfa38f87240ad67690c795689
Guillaume Etendard <guillaumetendard@gmail.com>
2017-10-02 15:27:00 +0200

    Add Task Component

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,31 @@
 {
     'use strict';
 
+    const Task = {
+        props: ['task', 'index'],
+        methods: {
+            editTask: function (task) {
+                this.$emit('edit-task', task);
+            },
+
+            deleteTask: function (task) {
+                this.$emit('delete-task', task);
+            }
+        },
+        template: `
+            <li class="collection-item">
+                <input type="checkbox" :id="'t_' + (index + 1)" :checked="task.isDone" v-on:click="editTask(task)">
+                <label :for="'t_' + (index + 1)">{{ task.title }}</label>
+                <a href="#" class="link-delete" title="Supprimer cette tâche" v-on:click="deleteTask(task)">
+                    <i class="small material-icons">delete_forever</i>
+                </a>
+            </li>`
+    };
+
     let vm = new Vue(
         {
             el: 'main#app',
+            components: {'task': Task},
 
             data: {
                 tasks: JSON.parse(localStorage.getItem('tasks')) || [],
